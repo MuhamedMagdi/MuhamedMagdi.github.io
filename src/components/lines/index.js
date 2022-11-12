6324343fa449689d829b156116d2190f54e47b7a
magdi <m49dii@gmail.com>
2022-11-12 19:20:48 +0200

    line breake for pages

---
+++ src/components/lines/index.js
@@ -0,0 +1,12 @@
+const Lines = () => {
+    require('./index.css');
+    return (
+        <>
+            <div className="line line-white margin-top slide-animate"> </div>
+            <div className="line line-red space slide-animate"> </div>
+            <div className="line line-red margin-bottom slide-animate"> </div>
+        </>
+    );
+};
+
+export default Lines;

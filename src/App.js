c366d2a753a9df4be4f7684b907e9fbd5b89bda1
magdi <m49dii@gmail.com>
2022-11-20 15:21:52 +0200

    fix: update notify badge
    reduce the time in takes to show up
    make it disapear only if you hover the text, it was disapearing before
    when you hover the line

--- src/App.js
+++ src/App.js
@@ -19,27 +19,30 @@ const App = () => {
             </Helmet>
             <div className="App">
                 <h1 className="home-font">
-                    <div className="slide-animate home-link" onMouseEnter={() => setCanShow(false)}>
+                    <div className="slide-animate home-link">
                         <Link to="about/">
-                            <div className="about content white" />
+                            <div
+                                className="about content white"
+                                onMouseEnter={() => setCanShow(false)}
+                            />
                         </Link>
                         <div className="about-line" />
                     </div>
-                    <div
-                        className="slide-animate home-link delay15"
-                        onMouseEnter={() => setCanShow(false)}
-                    >
+                    <div className="slide-animate home-link delay15">
                         <Link to="projects/">
-                            <div className="projects content red" />
+                            <div
+                                className="projects content red"
+                                onMouseEnter={() => setCanShow(false)}
+                            />
                         </Link>
                         <div className="projects-line" />
                     </div>
-                    <div
-                        className="slide-animate home-link delay30"
-                        onMouseEnter={() => setCanShow(false)}
-                    >
+                    <div className="slide-animate home-link delay30">
                         <Link to="contact/">
-                            <div className="contact content red" />
+                            <div
+                                className="contact content red"
+                                onMouseEnter={() => setCanShow(false)}
+                            />
                         </Link>
                         <div className="contact-line" />
                     </div>
